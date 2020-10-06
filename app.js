87f32740e2cea87e669a778ed652b91f3cb5d8a7
Mohammad Ahnaf Al-Rahmany <69469668+mohammadaahnaaf@users.noreply.github.com>
2020-10-07 01:13:02 +0600

    uploaded all files

--- app.js
+++ app.js
@@ -1,18 +1,29 @@
-const express = require("express")
-const data = require("./config.json")
+//jshint everson: 7
 
+const express = require("express");
+const data = require("./config.json");
+const bodyParser = require("body-parser");
+var items = [];
 const app = express();
+
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
 
-app.get("/", function(req, res) {
 
-  res.render("index", data)
+
+app.get("/", function(req, res) {
+  res.render("index", data);
+});
+
+app.post("/", function(req, res) {
+  var item = req.body.newItem;
+  items.push(item);
+
+  res.redirect("/");
 });
 
 
-
-console.log(data)
+console.log(data);
 app.listen(3000, function() {
-  console.log("Server is running on port 3000")
+  console.log("Server is running on port 3000");
 });
